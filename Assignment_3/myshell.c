c9f0e3009c8e7f4c5c09380a9be9eac1685ce7f5
Benker <lyleungad@connect.ust.hk>
2019-03-09 01:01:59 +0800

    support IO file name get and new parser plug to myshell

--- Assignment_3/myshell.c
+++ Assignment_3/myshell.c
@@ -15,9 +15,30 @@ void process_cmd(char *cmdline);
 
 
 // benker added
+
+// default have 10 argument, including cmd itself
+typedef struct Command_ {
+
+    char* command;              // pointer to command
+    
+    int used;                   // index to the top arugment
+    int left;                   // left pointers in argument_list, default 10, NULL occupy 1
+    char **argument_list;       // array of pointer to arguments
+
+    char* output_file;          // output file if any
+    char* input_file;           // input file if any
+
+    struct Command_* next;      // pointing to next command
+
+} CMD;
+
+
 void freeDir();     // free memory allocated to store directory
-int parser();       // allocate memory to point to cmd and arg, retunr 1 on success
-void freeCmd();     // free memory on point to cmd and arg
+void freeCMD(CMD *p);     // free memory on linked list of cmd
+void autoFillArg(CMD *p);   // auto add memory when required for arg list
+void initCMD(CMD *p);       // init cmd object
+int commandCount(CMD *p);   // count # of commands
+CMD* parser(char* cmdline); // return link list
 extern char* get_current_dir_name(void);
 extern char *dirname(char *path);
 extern char *basename(char *path);
@@ -30,10 +51,9 @@ char *dirc = NULL;          // used to store the part except last
 char *base_ = NULL;         // points to base, do not free this
 char *dirc_ = NULL;         // points to dirc, do not free this
 
-char** command = NULL;     // pointer to command locations
-char*** argument = NULL;   // pointer to argument locations
-int* argument_count = NULL;     // pointer to argument number
-int command_count = 1;  // total command
+
+int DEFAULT_ARUGMENT_ELE = 5;    // default argument element size, at least one
+
 
 
 /* The main function implementation */
@@ -127,23 +147,16 @@ void process_cmd(char *cmdline)
     else {
 
 
-        parser(cmdline);
+        CMD *cmd = parser(cmdline);
 
         int i;
-        // int j;
-        // printf("%d\n", command_count);
-        // printf("%s\n", cmdline);
-        // for(i=0; i<command_count; ++i) {
-        //     for(j=0; j<argument_count[i]; ++j) {
-        //         printf("Arg[%d][%d]:%s\n", i, j, argument[i][j]);
-        //     }
-        // }
-        // return;
+
         int count = 0;
         pid_t pid;
 
         // create enough pipe
         int **fds;
+        int command_count = commandCount(cmd);
         fds = calloc(command_count, sizeof(int*));
     
         for(i=0; i<command_count; ++i) {
@@ -163,18 +176,18 @@ void process_cmd(char *cmdline)
                     dup2(fds[count][1], 1);    // dup to stdout
 
                     // for running curr dir file
-                    char *temp_cmd = calloc(strlen(command[count])+strlen(directory)+2, sizeof(char));
+                    char *temp_cmd = calloc(strlen(cmd->command)+strlen(directory)+2, sizeof(char));
                     strcpy(temp_cmd, directory);
                     if(strlen(directory) != 1)
                         *(temp_cmd+strlen(directory)) = '/';
-                    strcpy(temp_cmd+strlen(directory)+1, command[count]);
-                    execv(temp_cmd, argument[count]);
+                    strcpy(temp_cmd+strlen(directory)+1, cmd->command);
+                    execv(temp_cmd, cmd->argument_list);
                     
                     // if curr dir has not such file, try run it from $PATH, and free temp
                     free(temp_cmd);
 
                     // fprintf(stderr, "1 executing %s\n", command[count]);
-                    execvp(command[count], argument[count]);
+                    execvp(cmd->command, cmd->argument_list);
                 }
                 // in case of last or first with only one command,
                 // it does read from prev pipe and write to current pipe, stdout
@@ -188,18 +201,18 @@ void process_cmd(char *cmdline)
 
 
                     // for running curr dir file
-                    char *temp_cmd = calloc(strlen(command[count])+strlen(directory)+2, sizeof(char));
+                    char *temp_cmd = calloc(strlen(cmd->command)+strlen(directory)+2, sizeof(char));
                     strcpy(temp_cmd, directory);
                     if(strlen(directory) != 1)
                         *(temp_cmd+strlen(directory)) = '/';
-                    strcpy(temp_cmd+strlen(directory)+1, command[count]);
-                    execv(temp_cmd, argument[count]);
+                    strcpy(temp_cmd+strlen(directory)+1, cmd->command);
+                    execv(temp_cmd, cmd->argument_list);
                     
                     // if curr dir has not such file, try run it from $PATH, and free temp
                     free(temp_cmd);
 
                     // fprintf(stderr, "2 executing %d, %s\n", count, command[count]);
-                    execvp(command[count], argument[count]);
+                    execvp(cmd->command, cmd->argument_list);
                 }
                 // in case of middle, it does read from prev and write to curr pipe
                 else {
@@ -214,20 +227,20 @@ void process_cmd(char *cmdline)
 
 
                     // for running curr dir file
-                    char *temp_cmd = calloc(strlen(command[count])+strlen(directory)+2, sizeof(char));
+                    char *temp_cmd = calloc(strlen(cmd->command)+strlen(directory)+2, sizeof(char));
                     strcpy(temp_cmd, directory);
                     if(strlen(directory) != 1)
                         *(temp_cmd+strlen(directory)) = '/';
-                    strcpy(temp_cmd+strlen(directory)+1, command[count]);
-                    execv(temp_cmd, argument[count]);
+                    strcpy(temp_cmd+strlen(directory)+1, cmd->command);
+                    execv(temp_cmd, cmd->argument_list);
                     
                     // if curr dir has not such file, try run it from $PATH, and free temp
                     free(temp_cmd);
 
                     // fprintf(stderr, "3 executing %s\n", command[count]);
-                    execvp(command[count], argument[count]);
+                    execvp(cmd->command, cmd->argument_list);
                 }
-                fprintf(stderr, "myshell: command not found: %s\n", command[count]);
+                fprintf(stderr, "myshell: command not found: %s\n", cmd->command);
                 exit(0);
             }
             else {
@@ -241,6 +254,7 @@ void process_cmd(char *cmdline)
                 wait(0);
                 // fprintf(stderr, "Parent%d\n", count);
                 ++count;
+                cmd = cmd->next;
             }
         }
 
@@ -300,164 +314,145 @@ void freeDir(){
 
 }
 
-// free the allocated memory
-void freeCmd() {
-    // deallocation of allocated memory
-    int i;
+// free cmd linked list
+void freeCMD(CMD *p) {
+    CMD *curr = p;
+    CMD *temp = p;
 
-    // free argument
-    if(argument != NULL) {
-        for(i=0; i<command_count; ++i) {
-            free(argument[i]);
-        }
-        free(argument);
-        argument = NULL;
+    while(curr != NULL) {
+        temp = curr;
+        curr = curr->next;
+        free(temp->argument_list);
+        free(temp);
     }
-    // free command
-    if(command != NULL) {
-        free(command);
-        command = NULL;
-    }
-        
-    
-    // free argument_count
-    if(argument_count != NULL) {
-        free(argument_count);
-        argument_count = NULL;
-    }
-
 }
-// parse command and argument, return 1 on success
-int parser(char *cmdline) {
-    
-    freeCmd();
 
-    command_count = 1;  // total command
-
-    char* temp = NULL;  // temp usage
-    // char* c = malloc(strlen((cmdline)+1) * sizeof(char));  // replace argument list in function
-    // strcpy(c, cmdline);
-    char *c = cmdline;
-    int read_command = 0;   // indicate that command read or not
-    int read_space = 0;     // indicate that space read or not
-    int arg_count = 0;      // for counting argument in each command, will be set to 0 if new command reach
-    int len = strlen(c);    // length of command line
-    int i;  // count the total command
-    int j;  // count command index
-
-    /* count the number of command */
-    for(i=0; i<len; ++i){
-        if(c[i] == '\'')
-            c[i] = '"';
-        if(c[i] == '|')
-            ++command_count;
+// auto fill the memory when required
+void autoFillArg(CMD *p) {
+    if(p->left == 0) {
+        p->left = DEFAULT_ARUGMENT_ELE;
+        p->argument_list = realloc(p->argument_list, p->used+1+DEFAULT_ARUGMENT_ELE);
+        return;
     }
+}
 
-    // allocate int to count argument size, and mem to cmd size
-    argument_count = calloc(command_count, sizeof(int));
-    command = calloc(command_count, sizeof(char*));
+// init CMD object
+void initCMD(CMD *p) {
+    *p = (CMD){.command=NULL, .used=0, .left=DEFAULT_ARUGMENT_ELE-1, .argument_list=calloc(DEFAULT_ARUGMENT_ELE, sizeof(char*)), .output_file=NULL, .input_file=NULL, .next=NULL};
+}
 
-    /* count the arguments in each command */
-    read_space = 1; // assume read space
-    read_command = 0; // assume not read command
-    for(i=0, j=0; i<len; ++i){  // j is current command index
-        // means next should be command
-        if(c[i] == '|') {
-            ++j;
-            read_command = 0;
-            read_space = 1;
-            continue;
-        }
-        //  means starting of 
-        else if(c[i] != ' ' && c[i] != '\t' && read_space == 1) {
-            if(read_command == 0) {
-                read_command = 1;
-                read_space = 0;
-                continue;
-            }
-            else {
-                // special case
-                if(c[i] == '"') {
-                    // find the next "
-                    temp = strchr(c+i+1, '"');
-                    if(temp == NULL) {
-                        // printf("Wrong syntax\n");
-                        freeCmd();
-                        return 0;
-                    }
-                    i = (temp-c)+1;
-                    read_space = 0;
-                    argument_count[j] += 1;
-                    continue;
+// count the number of command
+int commandCount(CMD *p) {
+    int i = 0;
+    while(p != NULL) {
+        ++i;
+        p = p->next;
+    }
+    return i;
+}
+
+// parse command and return linked list
+CMD* parser(char* cmdline) {
+
+    char* c = cmdline;
+
+    CMD *cmd = (CMD*)malloc(sizeof(CMD));
+    initCMD(cmd);
+
+    CMD *curr = cmd;
+
+    int len = strlen(cmdline);
+    int i;
+    int prev_space = 1;
+    char tempChar;
+    char* tempResult;
+    int output = 0;
+    int input = 0;
+    for(i=0; i<len; ++i) {
+
+        switch(c[i]) {
+
+            // end of string case
+            case '\0':
+                return cmd;
+
+            // continue string
+            case '\'':
+            case '"':
+                tempChar = c[i];
+                ++i;
+                tempResult = strchr(c+i, tempChar);
+                // not found, quit searching
+                if(tempResult == NULL) {
+                    return cmd;
                 }
+                *tempResult = '\0';
+                autoFillArg(curr);
+                curr->argument_list[(curr->used)++] = c+i;
+                --(curr->left);
+                i = tempResult-c+1;
+                prev_space = 0;
+                break;
+
+            // next cmd
+            case '|':
+                c[i] = '\0';
+                curr->next = malloc(sizeof(CMD));
+                initCMD(curr->next);
+                curr = curr->next;
+                prev_space = 1;
+                break;
+
+            // TODO
+            case '>':
+                c[i] = '\0';
+                output = 1;
+                break;
+            case '<':
+                c[i] = '\0';
+                input = 1;
+                break;
+
+            case ' ':
+                c[i] = '\0';
+                prev_space = 1;
+                break;
+            
+            // char
+            default:
+                if(prev_space == 0)
+                    break;
+                prev_space = 0;
+
+                // command
+                if(curr->command == NULL) {
+                    curr->command = c+i;
+                    autoFillArg(curr);
+                    curr->argument_list[(curr->used)++] = c+i;
+                    --(curr->left);
+                }
+                // argument
                 else {
-                    read_space = 0;
-                    argument_count[j] += 1;
-                    continue;
-                }
-            }
-        }
-        else if(c[i] == ' ' || c[i] == '\t') {
-            read_space = 1;
-        }
-    }
- 
-    /* initialize the array of string pointers points to arg */
-    argument = calloc(command_count, sizeof(char**));
-    for(i=0; i<command_count; ++i) {
-        argument_count[i]+=2;
-        argument[i] = calloc(argument_count[i], sizeof(char*));
-    }
-
-
-    read_command = 0;    // used in determind command got or not
-    read_space = 1;   // used for check first char   
-    /* assign pointers value point to cmd and arg */
-    for(i=0, j=0, arg_count=0; i<len ;++i) {
-        if(c[i] == '|') {
-            arg_count = 0; // set arg count to 0
-            read_command = 0;   // set not read command
-            read_space = 1;     // set read space as true
-            c[i] = '\0';        // set null
-            ++j;                // next command
-            continue;
-        }
-        else if(c[i] != ' ' && c[i] != '\t' && read_space == 1){
-            if(read_command == 0){
-                command[j] = (char*)c+i;                 // set command
-                argument[j][arg_count++] = (char*)c+i;   // set argument
-                read_command = 1;
-                read_space = 0;
-                continue;
-            }
-            else{
-                // special case
-                if(c[i] == '"') {
-                    // first saw "
-                    c[i] = '\0';
-                    argument[j][arg_count++] = (char*)c+i+1;
-                    temp = strchr(c+i+1, '"');
-                    if(temp == NULL){
-                        // printf("wrong syntax\n");
-                        freeCmd();
-                        return 0;
+                    if(input == 1) {
+                        input = 0;
+                        curr->input_file = c+i;
+                        break;
                     }
-                    *temp = '\0';
-                    i = (temp-c)+1;
-                    read_space = 0;
-                    continue;
+                    else if(output == 1) {
+                        output = 0;
+                        curr->output_file = c+i;
+                        break;
+                    }
+                    else {
+                        autoFillArg(curr);
+                        curr->argument_list[curr->used++] = c+i;
+                        --(curr->left);
+                    }
                 }
-                else{
-                    argument[j][arg_count++] = c+i;
-                    read_space = 0;
-                    continue;
-                }
-            }
+                break;
+
         }
-        else if(c[i] == ' ' || c[i] == '\t'){
-            read_space = 1;
-            c[i] = '\0';
-        }
-    } 
-    return 1;
+
+    }
+    return cmd;
 }
--- Assignment_3/parser.c
+++ Assignment_3/parser.c
@@ -6,24 +6,15 @@
 #include <sys/types.h>
 
 
-char** command = NULL;     // pointer to command locations
-char*** argument = NULL;   // pointer to argument locations
-int* argument_count = NULL;     // pointer to argument number
-int command_count = 1;  // total command
-
-int parser(char *cmdline);
-void freeCmd();
-
-
-int DEFAULT_ARUGMENT_ELE = 10;    // default argument element size
+int DEFAULT_ARUGMENT_ELE = 5;    // default argument element size, at least one
 
 // default have 10 argument, including cmd itself
 typedef struct Command_ {
 
     char* command;              // pointer to command
     
-    int used;                   // space used
-    int left;                   // left pointers in argument_list, default 10
+    int used;                   // index to the top arugment
+    int left;                   // left pointers in argument_list, default 10, NULL occupy 1
     char **argument_list;       // array of pointer to arguments
 
     char* output_file;          // output file if any
@@ -32,8 +23,39 @@ typedef struct Command_ {
     struct Command_* next;      // pointing to next command
 
 } CMD;
-// init 
-//(CMD){.command=NULL, .used=0, .left=DEFAULT_ARUGMENT_ELE, .argument_list=malloc(sizeof(char*)*DEFAULT_ARUGMENT_ELE), .output_file=NULL, .input_file=NULL, .next=NULL};
+
+int commandCount(CMD *p) {
+    int i = 0;
+    while(p != NULL) {
+        ++i;
+        p = p->next;
+    }
+    return i;
+}
+
+void initCMD(CMD *p) {
+    *p = (CMD){.command=NULL, .used=0, .left=DEFAULT_ARUGMENT_ELE-1, .argument_list=calloc(DEFAULT_ARUGMENT_ELE, sizeof(char*)), .output_file=NULL, .input_file=NULL, .next=NULL};
+}
+
+void autoFillArg(CMD *p) {
+    if(p->left == 0) {
+        p->left = DEFAULT_ARUGMENT_ELE;
+        p->argument_list = realloc(p->argument_list, p->used+1+DEFAULT_ARUGMENT_ELE);
+        return;
+    }
+}
+
+void freeCMD(CMD *p) {
+    CMD *curr = p;
+    CMD *temp = p;
+
+    while(curr != NULL) {
+        temp = curr;
+        curr = curr->next;
+        free(temp->argument_list);
+        free(temp);
+    }
+}
 
 
 CMD* newparser(char* cmdline) {
@@ -41,7 +63,7 @@ CMD* newparser(char* cmdline) {
     char* c = cmdline;
 
     CMD *cmd = (CMD*)malloc(sizeof(CMD));
-    *cmd = (CMD){.command=NULL, .used=0, .left=DEFAULT_ARUGMENT_ELE, .argument_list=malloc(sizeof(char*)*DEFAULT_ARUGMENT_ELE), .output_file=NULL, .input_file=NULL, .next=NULL};
+    initCMD(cmd);
 
     CMD *curr = cmd;
 
@@ -50,6 +72,8 @@ CMD* newparser(char* cmdline) {
     int prev_space = 1;
     char tempChar;
     char* tempResult;
+    int output = 0;
+    int input = 0;
     for(i=0; i<len; ++i) {
 
         switch(c[i]) {
@@ -69,6 +93,7 @@ CMD* newparser(char* cmdline) {
                     return cmd;
                 }
                 *tempResult = '\0';
+                autoFillArg(curr);
                 curr->argument_list[(curr->used)++] = c+i;
                 --(curr->left);
                 i = tempResult-c+1;
@@ -79,14 +104,20 @@ CMD* newparser(char* cmdline) {
             case '|':
                 c[i] = '\0';
                 curr->next = malloc(sizeof(CMD));
-                *(curr->next) = (CMD){.command=NULL, .used=0, .left=DEFAULT_ARUGMENT_ELE, .argument_list=malloc(sizeof(char*)*DEFAULT_ARUGMENT_ELE), .output_file=NULL, .input_file=NULL, .next=NULL};
+                initCMD(curr->next);
                 curr = curr->next;
                 prev_space = 1;
                 break;
 
             // TODO
             case '>':
+                c[i] = '\0';
+                output = 1;
+                break;
             case '<':
+                c[i] = '\0';
+                input = 1;
+                break;
 
             case ' ':
                 c[i] = '\0';
@@ -102,13 +133,27 @@ CMD* newparser(char* cmdline) {
                 // command
                 if(curr->command == NULL) {
                     curr->command = c+i;
+                    autoFillArg(curr);
                     curr->argument_list[(curr->used)++] = c+i;
                     --(curr->left);
                 }
                 // argument
                 else {
-                    curr->argument_list[curr->used++] = c+i;
-                    --(curr->left);
+                    if(input == 1) {
+                        input = 0;
+                        curr->input_file = c+i;
+                        break;
+                    }
+                    else if(output == 1) {
+                        output = 0;
+                        curr->output_file = c+i;
+                        break;
+                    }
+                    else {
+                        autoFillArg(curr);
+                        curr->argument_list[curr->used++] = c+i;
+                        --(curr->left);
+                    }
                 }
                 break;
 
@@ -121,7 +166,7 @@ CMD* newparser(char* cmdline) {
 
 int main() {
 
-    char cmdline[100] = "cat input.txt|grep '\"haha hehe\"   '";
+    char cmdline[100] = "./test < input.txt | head -n 3 | wc -l > output.txt";
 
 
     
@@ -129,191 +174,24 @@ int main() {
 
     while(cmd != NULL) {
 
-        printf("Cmd:%s\n", cmd->command);
+        printf("Cmd: [%s]\n", cmd->command);
+        if(cmd->input_file != NULL) {
+            printf("\tIn: [%s]\n", cmd->input_file);
+        }
+        if(cmd->output_file != NULL) {
+            printf("\tOut: [%s]\n", cmd->output_file);
+        }
         int i;
         for(i=0; i<cmd->used; ++i) {
-            printf("\tArg:%s\n", cmd->argument_list[i]);
+            printf("\tArg: [%s]\n", cmd->argument_list[i]);
         }
+
+        printf("\t\tused arg: [%d], left arg: [%d]\n", cmd->used, cmd->left);
         printf("\n");
         cmd = cmd->next;
     }
 
-    // parser(cmdline);
-    
-    // int i;
-    // int j;
-    // printf("%d\n", command_count);
-    // printf("%s\n", cmdline);
-    // for(i=0; i<command_count; ++i) {
-    //     printf("Cmd[%d]:%s\n", i, command[i]);
-    //     for(j=0; j<argument_count[i]; ++j) {
-    //         printf("Arg[%d][%d]:%s\n", i, j, argument[i][j]);
-    //     }
-    //     printf("\n");
-    // }
+    freeCMD(cmd);
 
     return 0;
-}
-
-// free the allocated memory
-void freeCmd() {
-    // deallocation of allocated memory
-    int i;
-
-    // free argument
-    if(argument != NULL) {
-        for(i=0; i<command_count; ++i) {
-            free(argument[i]);
-        }
-        free(argument);
-        argument = NULL;
-    }
-    // free command
-    if(command != NULL) {
-        free(command);
-        command = NULL;
-    }
-        
-    
-    // free argument_count
-    if(argument_count != NULL) {
-        free(argument_count);
-        argument_count = NULL;
-    }
-
-}
-
-// parse command and argument, return 1 on success
-int parser(char *cmdline) {
-    
-    freeCmd();
-
-    command_count = 1;  // total command
-
-    char* temp = NULL;  // temp usage
-    // char* c = malloc(strlen((cmdline)+1) * sizeof(char));  // replace argument list in function
-    // strcpy(c, cmdline);
-    char *c = cmdline;
-    int read_command = 0;   // indicate that command read or not
-    int read_space = 0;     // indicate that space read or not
-    int arg_count = 0;      // for counting argument in each command, will be set to 0 if new command reach
-    int len = strlen(c);    // length of command line
-    int i;  // count the total command
-    int j;  // count command index
-
-    /* count the number of command */
-    for(i=0; i<len; ++i){
-        if(c[i] == '\'')
-            c[i] = '"';
-        if(c[i] == '|')
-            ++command_count;
-    }
-
-    // allocate int to count argument size, and mem to cmd size
-    argument_count = calloc(command_count, sizeof(int));
-    command = calloc(command_count, sizeof(char*));
-
-    /* count the arguments in each command */
-    read_space = 1; // assume read space
-    read_command = 0; // assume not read command
-    for(i=0, j=0; i<len; ++i){  // j is current command index
-        // means next should be command
-        if(c[i] == '|') {
-            ++j;
-            read_command = 0;
-            read_space = 1;
-            continue;
-        }
-        //  means starting of 
-        else if(c[i] != ' ' && c[i] != '\t' && read_space == 1) {
-            if(read_command == 0) {
-                read_command = 1;
-                read_space = 0;
-                continue;
-            }
-            else {
-                // special case
-                if(c[i] == '"') {
-                    // find the next "
-                    temp = strchr(c+i+1, '"');
-                    if(temp == NULL) {
-                        // printf("Wrong syntax\n");
-                        freeCmd();
-                        return 0;
-                    }
-                    i = (temp-c)+1;
-                    read_space = 0;
-                    argument_count[j] += 1;
-                    continue;
-                }
-                else {
-                    read_space = 0;
-                    argument_count[j] += 1;
-                    continue;
-                }
-            }
-        }
-        else if(c[i] == ' ' || c[i] == '\t') {
-            read_space = 1;
-        }
-    }
- 
-    /* initialize the array of string pointers points to arg */
-    argument = calloc(command_count, sizeof(char**));
-    for(i=0; i<command_count; ++i) {
-        argument_count[i]+=2;
-        argument[i] = calloc(argument_count[i], sizeof(char*));
-    }
-
-
-    read_command = 0;    // used in determind command got or not
-    read_space = 1;   // used for check first char   
-    /* assign pointers value point to cmd and arg */
-    for(i=0, j=0, arg_count=0; i<len ;++i) {
-        if(c[i] == '|') {
-            arg_count = 0; // set arg count to 0
-            read_command = 0;   // set not read command
-            read_space = 1;     // set read space as true
-            c[i] = '\0';        // set null
-            ++j;                // next command
-            continue;
-        }
-        else if(c[i] != ' ' && c[i] != '\t' && read_space == 1){
-            if(read_command == 0){
-                command[j] = (char*)c+i;                 // set command
-                argument[j][arg_count++] = (char*)c+i;   // set argument
-                read_command = 1;
-                read_space = 0;
-                continue;
-            }
-            else{
-                // special case
-                if(c[i] == '"') {
-                    // first saw "
-                    c[i] = '\0';
-                    argument[j][arg_count++] = (char*)c+i+1;
-                    temp = strchr(c+i+1, '"');
-                    if(temp == NULL){
-                        // printf("wrong syntax\n");
-                        freeCmd();
-                        return 0;
-                    }
-                    *temp = '\0';
-                    i = (temp-c)+1;
-                    read_space = 0;
-                    continue;
-                }
-                else{
-                    argument[j][arg_count++] = c+i;
-                    read_space = 0;
-                    continue;
-                }
-            }
-        }
-        else if(c[i] == ' ' || c[i] == '\t'){
-            read_space = 1;
-            c[i] = '\0';
-        }
-    } 
-    return 1;
 }
