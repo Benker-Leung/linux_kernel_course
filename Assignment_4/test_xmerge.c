830c07a2b72f98b2bf41427c0f1a44a5f2aac875
Benker <lyleungad@connect.ust.hk>
2019-03-28 20:14:38 +0800

    fix bug that xp.ofile_count is null

--- Assignment_4/test_xmerge.c
+++ Assignment_4/test_xmerge.c
@@ -27,68 +27,86 @@ int main(int argc, char* argv[]) {
         exit(-1);
     }
 
-    int opt = 0;
-    int startOpt = 0;           // all files starting from here
-    int i = 0;
-    int temp = 0;
-    int defaultOflags = 1;
-    int defaultMode = 1;
-    struct xmerge_param xp;
+    long res = -1;                   // used to store the return value
+    int opt = 0;                // used for getopt to temp store the option char
+    int startOpt = 0;           // all files starting from here (index of argv)
+    int i = 0;                  // for counting
+    int temp = 0;               // for temp usage
+    int defaultOflags = 1;      // assume use default oflags, if not set it to 0
+    int defaultMode = 1;        // assume use default mode, if not set it to 0
+    struct xmerge_param xp;     // use to store the param that used by kernel space
+
+    // set the xmerge_param default values
     xp.oflags = 0;
     xp.mode = 0;
     xp.num_files = 0;
+    xp.ofile_count = calloc(1, sizeof(int));
 
     // parse the command
     while((opt=getopt(argc, argv, "acm")) != -1) {
         switch(opt){
-            case 'a':
-                printf("got a\n");
+            case 'a':       // append case
+                printf("append mode accepted\n");
                 defaultOflags = 0;
                 xp.oflags |= O_APPEND;
                 break;
-            case 'c':
-                printf("got c\n");
+            case 'c':       // create case
+                printf("creat mode accepted\n");
                 defaultOflags = 0;
                 xp.oflags |= O_CREAT;
                 break;
-            case 'm':
-                printf("got m\n");
-                defaultMode = 0;
-                temp = atoi(argv[optind]);
-                i = temp/100; temp -= i*100;
-                xp.mode |= i;
-                xp.mode <<= 3;
-                i = temp/10; temp -= i*10;
-                xp.mode |= i; 
-                i = (temp%10);
-                xp.mode <<= 3;
-                xp.mode |= i;
+            case 'm':       // change mode case
+                temp = atoi(argv[optind]);  // get the mode in base 10
+
+                for(i=0; i<3; ++i) {
+                    defaultMode = temp%10;
+                    if(defaultMode > 7) {
+                        fprintf(stderr, "Error mode bit detected!\n");
+                        exit(-1);
+                    }
+                    defaultMode <<= (i*3);
+                    xp.mode |= defaultMode;
+                    temp /= 10;
+                }
+                defaultMode = 0;        // set to 0 indicate not to use default mode
                 startOpt = optind + 1;
+                printf("change mode of file to [%d](in integer base 10)\n", xp.mode);
                 break;
             default:
-                fprintf(stderr, "Usage: %s [-a] [-c] [-m bits] outfile infile1 [infile2...]\n", argv[0]);
+                fprintf(stderr, "Usage: %s\t[flags] outfile infile infile2 ...\n", argv[0]);
+                fprintf(stderr, "\t[-a] O_APPEND flag attach\n");
+                fprintf(stderr, "\t[-c] O_CREAT flag attach\n");
+                fprintf(stderr, "\t[-m ???] change file mode bit\n");
                 exit(-1);
         }
     }
 
+    // set default oflags
     if(defaultOflags) {
         printf("use default oflags\n");
         xp.oflags = O_WRONLY | O_APPEND;
     }
+    // set default mode
     if(defaultMode) {
         printf("user default mode\n");
         xp.mode = S_IRUSR | S_IWUSR;
     }
 
+    // set the correct startOpt
     startOpt = startOpt==0?optind:startOpt;
-
+    // if beginning at startOpt(included), less than two argv, then quit
     if(startOpt+1 >= argc) {
         printf("Wrong length of command\n");
-        fprintf(stderr, "Usage: %s [-a] [-c] [-m bits] outfile infile1 [infile2...]\n", argv[0]);
+        fprintf(stderr, "Usage: %s\t[flags] outfile infile infile2 ...\n", argv[0]);
+        fprintf(stderr, "\t[-a] O_APPEND flag attach\n");
+        fprintf(stderr, "\t[-c] O_CREAT flag attach\n");
+        fprintf(stderr, "\t[-m ???] change file mode bit\n");
         exit(-1);
     }
 
+    // set the output file pointer
     xp.outfile = argv[startOpt++];
+    // allocate memory to char* array
     xp.infiles = calloc(argc-startOpt, sizeof(char*));
     i=0;
     while(startOpt<argc) {
@@ -96,8 +114,15 @@ int main(int argc, char* argv[]) {
         xp.num_files++;
     }
 
-    i = syscall(SYSCALL_NUM_XMERGE, &xp, sizeof(struct xmerge_param));
-
+    res = syscall(SYSCALL_NUM_XMERGE, &xp, sizeof(struct xmerge_param));
     free(xp.infiles);
+    free(xp.ofile_count);
+
+    if(res < 0) {
+        printf("[xmerge error]: [%d]\n", res);
+        exit(-1);
+    }
+
+
     return 0;
 }
--- Assignment_4/xmerge.c
+++ Assignment_4/xmerge.c
@@ -1,6 +1,7 @@
 #include <linux/kernel.h>
 #include <linux/syscalls.h>
 #include <asm/uaccess.h>
+
 #define BUFSIZE 256
 struct xmerge_param {
         __user char *outfile;
@@ -13,24 +14,28 @@ struct xmerge_param {
 SYSCALL_DEFINE2(xmerge, void*, args, size_t, argslen)
 {
         /* Local variable of the xmerge syscall */
-        struct xmerge_param xp;
-        __user char* tempFin = NULL;
-        char buf[BUFSIZE];
-        int writeByte = 0;
-        int readTemp = 0;
-        int writeTemp = 0;
-        int exitCode = 0;
-        int i = 0;
-        int fin = -1;
-        int fout = -1;
+        struct xmerge_param xp;         // used for store user space param
+        __user char* tempFin = NULL;    // used for temp store of input file pointer in user space
+        char buf[BUFSIZE];              // used to store char read
+        int writeByte = 0;              // count the write byte in total
+        int readTemp = 0;               // count the read byte each time
+        int writeTemp = 0;              // count the write byte each time
+        int exitCode = 0;               // exit integer return to user space program
+        int i = 0;                      // used for counting
+        int fin = -1;                   // used for file descriptor for input file
+        int fout = -1;                  // used for file descriptor for output file
+        int ofileCount = 0;             // used for counting merged number of file
+
         printk(KERN_INFO "the call begins\n");
+
         /* check the user_space pointers are valid */
-        // check args is valid or not
+
+        // check args(struct xmerge_param*) from user space is valid or not
         if(!access_ok(VERIFY_WRITE, args, argslen)) {
                 printk(KERN_INFO "args is invalid\n");
                 return -EFAULT;
         }
-        // copy from user to kernel of the struct
+        // copy from user to kernel of the (struct xmerge_param)
         if(copy_from_user(&xp, args, argslen)) {
                 printk(KERN_INFO "copying args to kernel fail\n");
                 return -EFAULT;
@@ -40,60 +45,88 @@ SYSCALL_DEFINE2(xmerge, void*, args, size_t, argslen)
                 printk(KERN_INFO "infiles is invalid\n");
                 return -EFAULT;
         }
+        // check int* ofile_count is valid or not
+        if(!access_ok(VERIFY_WRITE, xp.ofile_count, sizeof(int))) {
+                printk(KERN_INFO "infiles is invalid\n");
+                return -EFAULT;
+        }
+        /* end of checking the user_space pointers */
+
+
         /* Before the loop */
         mm_segment_t old_fs;
         old_fs = get_fs();
         set_fs(get_ds());
-        /* Write a loop to merge each input files */
-        // open the file to write first
+
+
+        // open the file to write if file exists
         fout = ksys_open(xp.outfile, xp.oflags | O_WRONLY, 0);
-        // if file not exist
+        // if file not exist, create a new one
         if(fout < 0) {
                 fout = ksys_open(xp.outfile, O_CREAT | O_WRONLY, 0);
+                // fail to create a file
                 if(fout < 0) {
                         printk(KERN_INFO "fail to open fout: [%d]\n", fout);
                         exitCode = fout;
                         goto EXIT;
                 }
         }
-        
-        // print each of the charater array address
+
+
+        /* Write a loop to merge each input files */
         for(i=0; i<xp.num_files; ++i) {
-                // get one char* of file to read
+
+                // get one input file pointer value from user space
                 if(copy_from_user(&tempFin, xp.infiles+i, sizeof(char*))) {
+                        // entered here if not correctly copy
                         printk(KERN_INFO "copying infiles[%d] fails\n", i);
                         exitCode = -EFAULT;
-                        ksys_close(fout);
-                        goto EXIT;
+                        goto CEXIT;
                 }
-                // open the file to read
-                fin = ksys_open(tempFin, O_RDONLY, 0);
+                
+                fin = ksys_open(tempFin, O_RDONLY, 0); // open the file to read
+                
                 if(fin < 0) {
+                        // entered if open fails
                         printk(KERN_INFO "fail to open fin: [%d], i:[%d]\n", fin, i);
                         exitCode = fin;
-                        ksys_close(fout);
-                        goto EXIT;
+                        goto CEXIT;
                 }
-                memset(buf, 0, BUFSIZE);
+
+                memset(buf, 0, BUFSIZE);        // clear buffer
+
                 // continuous read and write until read nothing
-                while((readTemp = ksys_read(fin, buf, BUFSIZE)) > 0) {
-                        writeTemp = ksys_write(fout, buf, readTemp);
+                while((readTemp = ksys_read(fin, buf, BUFSIZE)) > 0) {  // quit if readTemp < 0
+                        writeTemp = ksys_write(fout, buf, readTemp);    // record write byte this time
+                        // writeTemp shouldn't be less than 0, or error happens
                         if(writeTemp < 0) {
                                 printk(KERN_INFO "error during write\n");
                                 exitCode = writeTemp;
-                                ksys_close(fout);
-                                goto EXIT;
+                                goto CEXIT;
                         }
-                        writeByte += writeTemp;
+                        writeByte += writeTemp;         // count the total bytes wrote
                         memset(buf, 0, BUFSIZE);
                 }
-                ksys_close(fin);
+                ++ofileCount;           // increment merge file num
+                ksys_close(fin);        // close the file after merge
         }
-        ksys_close(fout);
-        ksys_chmod(xp.outfile, xp.mode);
+        
+        // maybe no error, then return bytes wrote to user
         exitCode = writeByte;
+
+        // copy the ofile_count back to user
+        if(copy_to_user(xp.ofile_count, &ofileCount, sizeof(int))) {
+                printk(KERN_INFO "error during copy to user\n");
+                exitCode = -EFAULT;
+                goto CEXIT;
+        }
+
+CEXIT:  // close fout and exit => CEXIT
+        ksys_chmod(xp.outfile, xp.mode);
+        ksys_close(fout);
         /* After all file operations, restore the old_fs */
- EXIT:
+
+EXIT:   // restore old_fs and exit
         set_fs(old_fs);
         printk(KERN_INFO "exit the syscall xmerge...\n");
         return exitCode;
